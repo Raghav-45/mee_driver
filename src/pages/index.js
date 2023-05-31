06a8fa6eecda73fe7b3fd9042d2c1bffed63a4f0
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-05-31 21:22:57 +0530

    adding driver seeing route

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { chakra, Tabs, TabList, TabPanels, Tab, TabPanel, Flex, Text, Center, Box, Input, Button, Stack, VStack, HStack, InputGroup, InputLeftElement } from '@chakra-ui/react'
+import { chakra, SimpleGrid, Tabs, TabList, TabPanels, Tab, TabPanel, Flex, Text, Center, Box, Input, Button, Stack, VStack, HStack, InputGroup, InputLeftElement, AspectRatio } from '@chakra-ui/react'
 import { useToast } from '@chakra-ui/react'
 import { AiFillCar } from 'react-icons/ai'
 import { FiPackage } from 'react-icons/fi'
@@ -16,10 +16,23 @@ import { useEffect, useState } from 'react'
 import { useAuth } from '../../contexts/AuthContext'
 import { useRouter } from 'next/router'
 
+import { useJsApiLoader, GoogleMap, Marker, Autocomplete, DirectionsService, DirectionsRenderer } from "@react-google-maps/api";
+
 export default function Home() {
   const toast = useToast()
   const { currentUser } = useAuth()
   const router = useRouter()
+
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: 'AIzaSyB0f0o77WzVWMIXX69u0oJL8zyKPKSsAEA',
+    libraries: ['places'],
+  })
+
+  const center = { lat: 28.659051, lng: 77.113777 }
+  const mapOptions = {zoomControl: false, streetViewControl: false, mapTypeControl: false, fullscreenControl: false}
+  const [directionsResponse, setDirectionsResponse] = useState(null)
+  const [distance, setDistance] = useState('')
+  const [duration, setDuration] = useState('')
 
   const [driver, setDriver] = useState()
 
@@ -28,6 +41,8 @@ export default function Home() {
 
   const [watchForRealtimeChanges, setWatchForRealtimeChanges] = useState(true)
   const [myRide, setMyRide] = useState()
+
+  const [rideQueue, setRideQueue] = useState([])
 
   function TabChange(index) {
     index == 0 && toast({
@@ -48,6 +63,34 @@ export default function Home() {
       variant:"information",
     })
   }
+
+
+  async function calculateRoute(start, end) {
+    if (start === '' || end === '') { return }
+    // setIsCalculatingRoute(true)
+    const directionsService = new google.maps.DirectionsService()
+    const results = await directionsService.route({
+      origin: start,
+      destination: end,
+      travelMode: google.maps.TravelMode.DRIVING,
+    })
+    setDirectionsResponse(results)
+
+    // console.log(results)
+
+    setDistance(results.routes[0].legs[0].distance.text)
+    setDuration(results.routes[0].legs[0].duration.text)
+    // setIsCalculatingRoute(false)
+  }
+
+  function clearRoute() {
+    setDirectionsResponse(null)
+    setDistance('')
+    setDuration('')
+    originInputRef.current.value = ''
+    destinationInputRef.current.value = ''
+  }
+
 
   const getDriverDetails = async () => {
     const { error, data } = await supabase.from('profile_driver').select().eq('username', currentUser.user_metadata.username.toLowerCase()).maybeSingle()
@@ -75,40 +118,60 @@ export default function Home() {
     pingDriver()
   }, [driver])
 
+  useEffect(() => {
+    if (rideQueue.length > 0) {
+      const e = rideQueue[rideQueue.length - 1]
+      console.log(e)
+      toast({
+        title: 'New Ride',
+        // description: `from (${e.pickup_loc}) - (${e.drop_loc}) for ${e.fare}Rs`,
+        description: `${e.fare} Rs`,
+        status: 'info',
+        duration: 10000,
+        isClosable: false,
+        position: 'bottom-right',
+      })
+    }
+  }, [rideQueue])
+  
+
   const AcceptRide = async (payload) => {
     const { data, error } = await supabase.from('waiting_rides').update({ is_accepted: true })
-                                                                .match({id: payload.new.id,
-                                                                        person_id: payload.new.person_id,
-                                                                        driver_id: payload.new.driver_id,
-                                                                        booked_at: payload.new.booked_at,
-                                                                        started_at: payload.new.started_at,
-                                                                        leave_at: payload.new.leave_at,
-                                                                        fare: payload.new.fare,
-                                                                        pickup_loc: payload.new.pickup_loc,
-                                                                        drop_loc: payload.new.drop_loc})
-    console.log('gg', data, error, payload)
+                                                                .match({id: payload.id,
+                                                                        person_id: payload.person_id,
+                                                                        driver_id: payload.driver_id,
+                                                                        booked_at: payload.booked_at,
+                                                                        started_at: payload.started_at,
+                                                                        leave_at: payload.leave_at,
+                                                                        fare: payload.fare,
+                                                                        pickup_loc: payload.pickup_loc,
+                                                                        drop_loc: payload.drop_loc})
+    // console.log('gg', data, error, payload)
   }
 
   useEffect(() => {
     const sub = supabase.channel('any')
-      .on('postgres_changes', driver ? { event: 'INSERT', schema: 'public', table: 'waiting_rides', filter: `driver_id=eq.${driver.id}` } : { event: 'INSERT', schema: 'public', table: 'rides' }, payload => {
+      .on('postgres_changes', driver ? { event: 'INSERT', schema: 'public', table: 'waiting_rides', filter: `driver_id=eq.${driver.id}` } : { event: 'INSERT', schema: 'public', table: 'waiting_rides' }, payload => {
         console.log('Change received!', payload)
 
         if (payload.new.is_accepted != true) {
           console.log('i can get this ride', payload)
 
           // do necessary things
-          AcceptRide(payload)
+          // AcceptRide(payload)
+          calculateRoute(payload.new.pickup_loc, payload.new.drop_loc)
         }
 
-        watchForRealtimeChanges && toast({
-          title: `New ride - ${driver && driver.id}`,
-          description: `from (${payload.new.pickup_loc}) - (${payload.new.drop_loc}) for ${payload.new.fare}Rs`,
-          status: 'info',
-          duration: 10000,
-          isClosable: false,
-          position: 'bottom-right',
-        })
+        // watchForRealtimeChanges && toast({
+        //   title: `New ride - ${driver && driver.id}`,
+        //   description: `from (${payload.new.pickup_loc}) - (${payload.new.drop_loc}) for ${payload.new.fare}Rs`,
+        //   status: 'info',
+        //   duration: 10000,
+        //   isClosable: false,
+        //   position: 'bottom-right',
+        // })
+
+        watchForRealtimeChanges && setRideQueue(current => [...current, payload.new])
         // chats.includes(payload.new) && console.log('i already have', payload.new)
         // console.log(chats)
         // setChats((current) => [...current, payload.new])
@@ -124,11 +187,36 @@ export default function Home() {
     myRide && console.log('Got ride', myRide)
   }, [myRide])
 
+  if (!isLoaded) {
+    return <Box>Loading...</Box>
+  }
+
   return (
     <Box>
-      <chakra.pre p={4}>
+      <Box pt={4}>
+        <Box mb={4} position={'relative'} left={0} top={0} h={'200px'} w={'100%'} overflow={'hidden'} rounded={'xl'}>
+          <Box position={'absolute'} left={0} top={0} h={'100%'} w={'100%'}>
+            <GoogleMap center={center} zoom={17} mapContainerStyle={{width: '100%', height: '100%'}} options={mapOptions}>
+              {/* <Marker position={center} /> */}
+              {directionsResponse && <DirectionsRenderer directions={directionsResponse} />}
+            </GoogleMap>
+          </Box>
+        </Box>
+
+        <SimpleGrid height={'100%'} columns={2} spacing={4}>
+          {rideQueue.map((elem) => 
+            <AspectRatio key={elem.id} ratio={1 / 1}><Button onClick={() => {AcceptRide(elem).then(toast({title: 'New Ride', status: 'success', duration: 10000, isClosable: false, position: 'bottom-right'}))}} rounded={'xl'} bg={elem.is_accepted != true ? 'red.400' : 'blue.400'} colorScheme={elem.is_accepted != true ? 'red' : 'blue'}></Button></AspectRatio>
+          )}
+
+          {[...Array(4 - rideQueue.length).keys()].map((elem) => 
+            <AspectRatio key={elem} ratio={1 / 1}><Button rounded={'xl'} bg={'gray.300'} colorScheme='blackAlpha'></Button></AspectRatio>
+          )}
+        </SimpleGrid>
+      </Box>
+
+      {/* <chakra.pre p={4}>
         {myRide && <pre>{JSON.stringify(myRide, null, 2)}</pre>}
-      </chakra.pre>
+      </chakra.pre> */}
     </Box>
   )
 }
