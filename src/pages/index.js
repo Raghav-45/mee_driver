d8999ff03b0b367fd60f55d735c4ac3af570ef08
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-07-15 23:16:19 +0530

    Update index.js
    ride will stop showing on Screen if Someone Other Accepted it

--- src/pages/index.js
+++ src/pages/index.js
@@ -124,23 +124,63 @@ export default function Home() {
     }
   }, [rideQueue])
 
+  const rideisAccepted = (payload) => {
+    rideQueue.forEach((elem) => {
+      elem.id == payload.id && console.log('exists')
+    })
+  }
+
+  function checkIfRideExists(array, checkFor) {
+    for (var i = 0; i < array.length; i++) {
+      if (array[i].id == checkFor) {
+        console.log('this exists')
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  function removeIfRideExists(array, checkFor) {
+    const updated_array = []
+    for (var i = 0; i < array.length; i++) {
+      if (array[i].id == checkFor) {
+        console.log('this exists', i)
+        // return true;
+      } else {
+        updated_array.push(array[i])
+      }
+    }
+
+    return updated_array;
+  }
+
   useEffect(() => {
     const subscribe = supabase.channel('any')
       //TODO: i have to add Filter Here
       .on('postgres_changes', { event: '*', schema: 'public', table: 'waiting_rides_test' }, payload => {
         console.log('Change received!', payload)
 
-        if (payload.eventType == 'INSERT') {
+        if (payload.eventType == 'INSERT' && payload.new.is_accepted == null) {
           setRideQueue(current => [...current, payload.new])
         }
-        if (payload.eventType == 'UPDATE') {
-          updateRideIsAccepted(payload.new)
+        if (payload.eventType == 'UPDATE' && payload.new.is_accepted == true) {
+          if (payload.new.is_accepted == true && payload.new.driver_id == currentUser.id) {
+            updateRideIsAccepted(payload.new)
+          }
+          if (payload.new.is_accepted == true && payload.new.driver_id != currentUser.id) {
+            setRideQueue(current => removeIfRideExists([...current], payload.new.id))
+            // updateRideIsAccepted(payload.new)
+          }
         }
+        // if (payload.eventType == 'UPDATE') {
+        //   updateRideIsAccepted(payload.new)
+        // }
       }).subscribe()
     return () => {
       supabase.removeChannel(subscribe)
     }
-  }, [watchForRealtimeChanges])
+  }, [watchForRealtimeChanges, currentUser])
 
   const GetDriverName = (props) => {
     const [userInfo, setUserInfo] = useState(null)
@@ -192,7 +232,7 @@ export default function Home() {
         </Box>
 
         <SimpleGrid height={'100%'} columns={2} spacing={4}>
-          {rideQueue.map((elem) => 
+          {rideQueue && rideQueue.map((elem) => 
             <AspectRatio key={elem.id} ratio={1 / 1}>
               <Button rounded={'xl'} bg={elem.is_accepted != true ? 'red.400' : 'blue.400'} colorScheme={elem.is_accepted != true ? 'red' : 'blue'} textColor={'white'} size={'sm'} onClick={() => {AcceptRide(elem)}}>
                 <GetDriverName uuid={String(elem.person_id)} />
